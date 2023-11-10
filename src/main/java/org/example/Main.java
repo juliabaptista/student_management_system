44749c5f45457c442880026885aa9b0f540844db
juliabaptista <juliabaptistadeoliveira@gmail.com>
2023-11-10 17:56:42 +0100

    feat: user management using arrays and loops     - implement CRUD operations, such as:       -> add       -> delete       -> display

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,13 +7,20 @@ public class Main {
     public static void main(String[] args) {
         UserManager userManager = new UserManager();
         CLIMenu cliMenu = new CLIMenu(userManager);
-/*
-//        test registration
+
+//        test registration and display users
         userManager.registerUser("jbptst", "password1", "Student");
+        userManager.registerUser("fbrugger", "password2", "Teacher");
+        userManager.displayUsers();
+
 //        test login
-        userManager.loginUser("jbptst", "password1");
-        userManager.loginUser("julia", "password1");
-*/
+        userManager.loginUser("jbptst", "password1", "Student");
+        userManager.loginUser("julia", "password1", "Teacher");
+
+//        test delete and display users
+        userManager.deleteUser("jbptst");
+        userManager.displayUsers();
+
         cliMenu.displayMenu();
     }
 }
--- src/main/java/org/example/UserManagement/UserManager.java
+++ src/main/java/org/example/UserManagement/UserManager.java
@@ -1,40 +1,92 @@
 package org.example.UserManagement;
 
-import java.util.HashMap;
+import java.util.Arrays;
 
 public class UserManager {
-    private HashMap<String, User> userDatabase;
+    //    using Arrays and Loops
+    private User[] users;
 
     public UserManager() {
-        this.userDatabase = new HashMap<>();
+        this.users = new User[10];
     }
 
     public void registerUser(String username, String password, String role) {
 //        check if the username already exists
-        if (userDatabase.containsKey(username)) {
+        if (getUserByUsername(username) != null) {
             System.out.println("Username already exists. Choose another username.");
         } else {
 //            in case not, create a new user and add it to the database
             User newUser = new User(username, password, role);
-            userDatabase.put(username, newUser);
+            addUser(newUser);
             System.out.println("Registration successful!");
         }
     }
 
-    public boolean loginUser(String username, String password) {
-//        check if the username already exists
-        if (userDatabase.containsKey(username)) {
-//            check if the password is correct
-            User user = userDatabase.get(username);
-            if (user.getPassword().equals(password)) {
+    public boolean loginUser(String username, String password, String role) {
+        for (int i = 0; i < users.length; i++) {
+            User user = users[i];
+            if (user != null
+                    && user.getUsername().equals(username)
+                    && user.getPassword().equals(password)
+                    && user.getRole().equals(role)) {
                 System.out.println("Login successful! Welcome, " + user.getUsername() + "!");
                 return true;
-            } else {
-                System.out.println("Incorrect password. Please try again.");
             }
-        } else {
-            System.out.println("Username not found. Please register first.");
         }
+        System.out.println("Login failed. Please check your credentials.");
         return false;
+    }
+
+    //    CRUD operations
+    private void addUser(User newUser) {
+//    find the first available index in the array
+        int i = findEmptyIndex();
+        if (i != -1) {
+            users[i] = newUser;
+        } else {
+//        i = -1 means the array is full
+            System.out.println("User limit reached. Unable to add more users.");
+        }
+    }
+
+    private int findEmptyIndex() {
+        for (int i = 0; i < users.length; i++) {
+            if (users[i] == null) {
+                return i;
+            }
+        }
+//        if the array is full, return -1
+        return -1;
+    }
+
+    public void deleteUser(String username) {
+        User userToDelete = getUserByUsername(username);
+        if (userToDelete != null) {
+//            delete the user by setting the array element to null
+            int index = Arrays.asList(users).indexOf(userToDelete);
+            users[index] = null;
+            System.out.println("User deleted successfully.");
+        } else {
+            System.out.println("User not found.");
+        }
+    }
+
+    private User getUserByUsername(String username) {
+        for (User user : users) {
+            if (user != null && user.getUsername().equals(username)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public void displayUsers() {
+        System.out.println("________ User List ________");
+        for (int i = 0; i < users.length; i++) {
+            User user = users[i];
+            if (user != null) {
+                System.out.println(user);
+            }
+        }
     }
 }
