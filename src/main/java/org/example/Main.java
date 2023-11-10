5972c76d1f3551f4357ec36f33fb46c5da0680f7
juliabaptista <juliabaptistadeoliveira@gmail.com>
2023-11-10 16:25:29 +0100

    feat: add CLI menu to main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,19 @@
 package org.example;
 
+import org.example.CLI.CLIMenu;
 import org.example.UserManagement.UserManager;
 
 public class Main {
     public static void main(String[] args) {
         UserManager userManager = new UserManager();
-
+        CLIMenu cliMenu = new CLIMenu(userManager);
+/*
 //        test registration
         userManager.registerUser("jbptst", "password1", "Student");
 //        test login
         userManager.loginUser("jbptst", "password1");
         userManager.loginUser("julia", "password1");
+*/
+        cliMenu.displayMenu();
     }
 }
